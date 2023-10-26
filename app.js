3135b5a920656ab1718a3de0786954405f619ea2
Devesh-23 <deveshpachauri23@gmail.com>
2023-10-26 22:31:57 +0530

    setting up the backend

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const task = require('./routes/task')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+//middleware
+app.use(express.json())
+
+//routing 
+app.get('/hello', (req, res) =>{
+    res.send("my first req from ther server");
+})
+
+app.use('/api/v1/tasks', task)
+const port = 3000;
+
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, console.log(`server is listing on ${port} ...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
